6a59f2a2016bd6d16ad9fc2c9e7aadcf408b8f95
Antony Watkins <arwatkins89@gmail.com>
2020-09-30 15:59:44 -0400

    Initial commit of rock paper scissors

---
+++ Rock Paper Scissors.js
@@ -0,0 +1,50 @@
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    return userInput;
+  } else {
+    console.log('Error!');
+    }
+  }
+  
+  function getComputerChoice() {
+    switch (Math.floor(Math.random()*3)) {
+      case 0:
+        return 'rock';
+        break;
+      case 1:
+        return 'scissors';
+        break;
+      case 2:
+        return 'paper';
+        break;
+        };
+  }
+  
+  function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+      return 'The game is a tie!';
+    }
+    if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+    if (userChoice === 'paper') {
+      if (computerChoice === 'scissors') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+    if (userChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+  }
+  console.log(determineWinner('paper', 'rock'));
