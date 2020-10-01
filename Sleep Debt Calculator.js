09c62b7adf304d75b0f6a0333fec1049d6c3c499
Antony Watkins <arwatkins89@gmail.com>
2020-10-01 02:58:35 -0400

    adding calculations and function calls

--- Sleep Debt Calculator.js
+++ Sleep Debt Calculator.js
@@ -1,3 +1,4 @@
+//This can also be done with an if/else statement for each day. e.g. if (day === 'monday'){ return 8;} else if...
 const getSleepHours = day => {
     switch(day) {
       case 'monday':
@@ -23,7 +24,7 @@ const getSleepHours = day => {
         break;
      }    
     };
-  
+  //Adding the sleep hours together! Note it is adding the getSleepHours function with each day perameter in place. Note the lack of curly braces. This is an "implicit return"
   const getActualSleepHours = () => 
       getSleepHours('monday') +
       getSleepHours('tuesday') +
@@ -33,11 +34,26 @@ const getSleepHours = day => {
       getSleepHours('saturday') +
       getSleepHours('sunday')
   ;
+
+  //What I want my sleep to be
   const getIdealSleepHours = () => {
     const idealHours = 8;
     return idealHours * 7;
   }
   
+  const calculateSleepDebt = () => {
+    const actualSleepHours = getActualSleepHours();
+    const idealSleepHours = getIdealSleepHours();
+    if (actualSleepHours === idealSleepHours) {
+      console.log('Good job!');
+    } else if (actualSleepHours > idealSleepHours) {
+      console.log('Lazy bones!');
+    }else {
+      console.log('You got ' + (idealSleepHours - actualSleepHours) + 'hour(s) less sleep than you needed this week. Get some sleep!');
+    }
+  };
+
+  //These are the tests
   console.log(getActualSleepHours());
   console.log(getIdealSleepHours());
   
