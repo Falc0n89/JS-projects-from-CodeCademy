242ce90aeb1ee1366aed7a1d61498d26ec183af3
Antony Watkins <arwatkins89@gmail.com>
2020-10-06 16:54:03 -0400

    Added first function and started second

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -3,4 +3,12 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random()* 8 + 1);
+}
 
+const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+    
+}
+
+console.log(generateTarget());
