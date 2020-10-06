671749171f6e65c63f163e4321fabdc8e31c5da5
Antony Watkins <arwatkins89@gmail.com>
2020-10-06 17:17:28 -0400

    Added compare guesses function

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -7,8 +7,14 @@ const generateTarget = () => {
     return Math.floor(Math.random()* 8 + 1);
 }
 
-const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
-    
+const compareGuesses = (humanGuess, computerGuess, secretTarget) => {
+ if (Math.abs(humanGuess - secretTarget) <= Math.abs(computerGuess - secretTarget)) {
+   return true;
+ } else if(Math.abs(humanGuess - secretTarget) > Math.abs(computerGuess - secretTarget)) {
+   return false;
+ }
 }
+
+console.log(compareGuesses(1, 2, 1));
 
 console.log(generateTarget());
