0634306361a802625b9b79424ab4d969ee58836d
Antony Watkins <arwatkins89@gmail.com>
2020-10-30 01:36:04 -0400

    added basic for loop project

---
+++ Basic for loop.js
@@ -0,0 +1,7 @@
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+//This checks that the index is less than the length of this array, and once it is more, it stops looping!
+for (let i = 0; i < vacationSpots.length; i++)
+{
+  console.log('I would love to visit ' +vacationSpots[i]);
+}
