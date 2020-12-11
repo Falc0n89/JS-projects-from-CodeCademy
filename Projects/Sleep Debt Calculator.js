b06c62f0f2eb2ab86ced15f19a999ca8b38b2b6f
Antony Watkins <arwatkins89@gmail.com>
2020-12-11 11:55:55 -0500

    Cleaned up files

---
+++ Projects/Sleep Debt Calculator.js
@@ -0,0 +1,64 @@
+//This can also be done with an if/else statement for each day. e.g. if (day === 'monday'){ return 8;} else if...
+const getSleepHours = day => {
+    switch(day) {
+      case 'monday':
+        return 8;
+        break;
+      case 'tuesday':
+        return 8;
+        break;
+      case 'wednesday':
+        return 9;
+        break;
+      case 'thursday':
+        return 9;
+        break;
+      case 'friday':
+        return 7;
+        break;
+      case 'saturday':
+        return 10;
+        break;
+      case 'sunday':
+        return 9;
+        break;
+     }    
+    };
+  //Adding the sleep hours together! Note it is adding the getSleepHours function with each day perameter in place. Note the lack of curly braces. This is an "implicit return"
+  const getActualSleepHours = () => 
+      getSleepHours('monday') +
+      getSleepHours('tuesday') +
+      getSleepHours('wednesday') +
+      getSleepHours('thursday') +
+      getSleepHours('friday') +
+      getSleepHours('saturday') +
+      getSleepHours('sunday')
+  ;
+
+  //What I want my sleep to be
+  const getIdealSleepHours = () => {
+    const idealHours = 8;
+    return idealHours * 7;
+  }
+  
+  const calculateSleepDebt = () => {
+    const actualSleepHours = getActualSleepHours();
+    const idealSleepHours = getIdealSleepHours();
+    if (actualSleepHours === idealSleepHours) {
+      console.log(' You got the recommended amount of sleep. Good job!');
+    } else if (actualSleepHours > idealSleepHours) {
+      console.log('You got ' +(actualSleepHours % idealSleepHours) + ' too many hours sleep. Lazy bones!');
+    }else {
+      console.log('You got ' + (idealSleepHours - actualSleepHours) + 'hour(s) less sleep than you needed this week. Get some sleep!');
+    }
+  };
+  
+  //Here is the function call to run the program
+  calculateSleepDebt();
+  
+  
+  
+  //These are the tests
+  console.log(getActualSleepHours());
+  console.log(getIdealSleepHours());
+  
